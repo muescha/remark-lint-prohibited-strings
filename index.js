0f76ed93dea76a6460aa42aba3717b8b50c9dda9
Muescha <184316+muescha@users.noreply.github.com>
2020-04-25 08:49:18 +0200

    fix position, multiple results, debuginfo

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 
 const rule = require('unified-lint-rule');
 const visit = require('unist-util-visit');
+const position = require('unist-util-position')
+const vfileLocation = require('vfile-location')
+
 
 module.exports = rule('remark-lint:prohibited-strings', prohibitedStrings);
 
 function testProhibited(val, content) {
-  let regexpString = '(\\.|@[a-z0-9/-]*)?';
+  let regexpString = '(\\.|@[a-zA-Z0-9/-]*)?';
 
   // If it starts with a letter, make sure it is a word break.
   if (/^\b/.test(val.no)) {
@@ -22,26 +25,58 @@ function testProhibited(val, content) {
   const re = new RegExp(regexpString, 'g');
 
   let result = null;
+  let results = []
   while (result = re.exec(content)) {
-    if (!result[1] && !result[3]) {
-      return result[2];
+    if (!result[1] && !result[3] && result[2] !== val.yes) {
+      results.push({
+        match: result[2],
+        startIndex: re.lastIndex - result[2].length,
+        endIndex: re.lastIndex,
+        debug: { s: regexpString, c: content }
+      });
     }
   }
 
-  return false;
+  return results;
 }
 
 function prohibitedStrings(ast, file, strings) {
+
+  const location = vfileLocation(file)
+
+
+  if(!strings){
+    return
+  }
+
   visit(ast, 'text', checkText);
 
   function checkText(node) {
+
     const content = node.value;
+    const offset = location.toOffset(position.start(node))
+
 
     strings.forEach((val) => {
-      const result = testProhibited(val, content);
-      if (result) {
-        file.message(`Use "${val.yes}" instead of "${result}"`, node);
+
+      const results = testProhibited(val, content);
+      if (results.length) {
+        console.log(node)
+        results.forEach(result => {
+          const position = {
+            start: location.toPosition(offset + result.startIndex),
+            end: location.toPosition(offset + result.endIndex)
+          }
+
+          const debugInfo = true ? ` \n\n${JSON.stringify(result.debug,null,'  ')}` : ``
+
+          const message = file.message(`Use "${val.yes}" instead of "${result.match}"${debugInfo}`, position);
+          message.id = id(val);
+        })
       }
     });
+  }
+  function id(val){
+    return val.id?val.id:val.yes
   }
 }
